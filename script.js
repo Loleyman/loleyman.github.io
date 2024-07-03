0de1bc8598e5b122f29810b9515cec59822bf421
Loleyman <67792660+Loleyman@users.noreply.github.com>
2024-07-03 23:36:23 +0300

    improved fun :)

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ const container = document.getElementById('container');
 const illuminatedItem = document.querySelector('.illuminated-item');
 const flashlight = document.querySelector('.flashlight');
 const blurFilter = document.querySelector('#blur-filter feGaussianBlur');
+const logoText = document.getElementById("logotext");
 
-
+var fontNum = 1;
 
 const lightRadius = 500;
 const flashlightOffset = lightRadius / 2;
 
 flashlight.style.width = flashlight.style.height = `${lightRadius}px`;
-const filterIntensity = 30;
+const filterIntensity = 40;
 blurFilter.setAttribute('stdDeviation', filterIntensity);
 
 const followMouseFlashlight = ({ clientX, clientY }) => {
@@ -28,4 +29,53 @@ function playd() {
   function playu() {
     var audiou = document.getElementById("audiou");
     audiou.cloneNode().play()
+    fontNum += 1;
+    switch(fontNum) {
+      case 2:
+        logoText.style.fontFamily = "Caveat,cursive";
+        break;
+      case 3:
+        logoText.style.fontFamily = "Lobster,sans-serif";
+        break;
+      case 4:
+        logoText.style.fontFamily = "Tiny5, sans-serif";
+        break;
+      case 5:
+        logoText.style.fontFamily = "Chakra Petch,sans-serif";
+        break;
+      case 6:
+        logoText.style.fontFamily = "LXGW WenKai TC,cursive";
+        break;
+      case 7:
+        logoText.style.fontFamily = "Margarine,sans-serif"; //
+        break;
+      case 8:
+        logoText.style.fontFamily = "Quicksand,sans-serif";
+        break;
+      case 9:
+        logoText.style.fontFamily = "Playfair Display,serif";
+        break;
+      case 10:
+        logoText.style.fontFamily = "Baskervville SC,serif";
+        break;
+      case 11:
+        logoText.style.fontFamily = "Ga Maamli,sans-serif";
+        break;
+      case 12:
+        logoText.style.fontFamily = "Fustat,sans-serif";
+        break;
+      case 13:
+        logoText.style.fontFamily = "Beiruti,sans-serif"; 
+        break;
+      case 14:
+        logoText.style.fontFamily = "Neucha,cursive";
+        break;
+      case 15:
+        logoText.style.fontFamily = "Nanum Pen Script,cursive";
+        break;
+      default:
+        logoText.style.fontFamily = "Shadows Into Light,cursive";
+        fontNum = 1;
+        break
+    }
   }
